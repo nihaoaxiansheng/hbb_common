4a71b028df43d57e67b5c3c910030a27717096a1
nihaoaxiansheng <1153627788@qq.com>
2025-10-16 15:53:30 +0800

    Update rendezvous server and public key constants

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["k00.top"];
+pub const RS_PUB_KEY: &str = "hCcDiwhDlkB1anqe753wTd28Ru8IVE9J24RGs1IHh+E=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
